a851492ac1b4bcdd36c6b5a24506f5b1d3a56155
Alexis Aguilera Borges <aguileraalexis@yahoo.com>
2020-08-15 10:34:13 -0400

    first commit

---
+++ HumberAdvancedCSharpFinalExamQ22/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HumberAdvancedCSharpFinalExamQ22
+{
+    //This class defines an event
+    public class ClassWithEvent
+    {
+         public event MyDelegate MyEvent;
+
+        //When this is called the event is launched
+        protected void OnDoSomething()
+        {
+            MyEventArgs args = new MyEventArgs();
+            args.SomeInfo = "som info";
+
+            MyEvent(this, args);
+        }
+
+    }
+
+    //event args with some data
+    public class MyEventArgs
+    {
+        public String SomeInfo { get; set; }
+    }
+
+    //the delegate
+    public delegate void MyDelegate(Object sender, MyEventArgs args);
+
+    //this class uses the previous and handles the event
+    public class EventUserClass
+    {
+
+        private ClassWithEvent myObjectWithEvents = new ClassWithEvent();
+
+        public EventUserClass()
+        {
+            myObjectWithEvents.MyEvent += MyEventHandler;
+        }
+
+        public void MyEventHandler(Object sender, MyEventArgs args)
+        {
+            Console.WriteLine(args.SomeInfo);
+        }
+
+    }
+
+}
